fd2b7faf1fffe96c62492f9dbe2c976e8cc7b73e
闫肖肖 <yanxiaoxiao@daoxila.com>
2018-05-16 11:46:14 +0800

    update

---
+++ wechat-mine/pages/login/index.js
@@ -0,0 +1,143 @@
+// pages/login/index.js
+// 1. 获取openID
+// 2. 获取用户头像，以及一些基本信息，是否要存储
+// 3. 实现ajax 请求
+const util = require("../../utils/util.js")
+const app = getApp()//获取应用信息
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    mobile: '',
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
+  },
+
+  setMobileValue: function (e) {
+    this.setData({ mobile: e.detail.value });
+  },
+  login:function(){
+    wx.request({
+      url: 'https://app.yxxit.com/design/test',
+      method: 'POST',
+      data: {
+        mobile: this.data.mobile
+      },
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success: function (res) {
+        console.log(res.data)
+        util.showSuccess("信息提交成功！")
+      }
+    })
+  },
+  getUserInfo: function (e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse) {
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+
+
+    wx.request({
+      url: 'https://app.yxxit.com/design/test',
+      method: 'POST',
+      data: {
+        x: 'a',
+        y: 'b'
+      },
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success: function (res) {
+        console.log(res.data)
+      }
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
